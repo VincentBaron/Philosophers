cc12102a212fab4ae07c5a23f98bcd57079de7f8
VincentBaron <vincentbaron1996@gmail.com>
2021-12-06 19:27:36 +0100

    good again with init forks

--- srcs/fill_mother.c
+++ srcs/fill_mother.c
@@ -6,7 +6,7 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 14:51:37 by vbaron            #+#    #+#             */
-/*   Updated: 2021/12/03 16:48:42 by vbaron           ###   ########.fr       */
+/*   Updated: 2021/12/06 19:24:05 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int fill_mother(t_gen *mother, char **av, int ac)
 {
+    int i;
+    
     pthread_mutex_init(&mother->write_mutex, NULL);
     mother->dead_philo = -1;
     mother->dead_philo = -1;
@@ -30,6 +32,9 @@ int fill_mother(t_gen *mother, char **av, int ac)
     mother->forks = NULL;
     mother->philo = (t_philo *)malloc(sizeof(t_philo) * mother->nb_philos);
     mother->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * mother->nb_philos);
+    i = -1;
+    while (++i < mother->nb_philos)
+        pthread_mutex_init(&mother->forks[i], NULL);
     if (!mother->philo || mother->forks)
         return (0);
     return (1);
--- srcs/lock_unlock_forks.c
+++ srcs/lock_unlock_forks.c
@@ -6,7 +6,7 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 10:20:49 by vbaron            #+#    #+#             */
-/*   Updated: 2021/12/03 17:23:09 by vbaron           ###   ########.fr       */
+/*   Updated: 2021/12/06 19:25:56 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,6 @@ void lock_forks(t_philo *philo)
 
 void unlock_forks(t_philo *philo)
 {
-    if (philo->id % 2 == 0)
-    {
-        pthread_mutex_unlock(philo->lfork);
-        pthread_mutex_unlock(philo->rfork);
-    }
-    else
-    {
-        pthread_mutex_unlock(philo->rfork);
-        pthread_mutex_unlock(philo->lfork);
-    }
+    pthread_mutex_unlock(philo->rfork);
+    pthread_mutex_unlock(philo->lfork);
 }
