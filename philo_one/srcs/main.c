905bc15ffab2125020e28bfbe0be8560480fc7de
VincentBaron <vincentbaron1996@gmail.com>
2021-12-08 17:12:12 +0100

    1 philo case managed

--- philo_one/srcs/main.c
+++ philo_one/srcs/main.c
@@ -6,37 +6,53 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 11:40:58 by vbaron            #+#    #+#             */
-/*   Updated: 2021/12/08 16:46:29 by vbaron           ###   ########.fr       */
+/*   Updated: 2021/12/08 17:10:19 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-int	main(int ac, char **av)
+void one_philo(t_gen *mother)
 {
-	t_gen	mother;
-	int		i;
+	long start_t;
+
+	safe_write(&mother->philo[0], FORK);
+	start_t = get_time();
+	while ((get_time() - start_t) < mother->t_die)
+		usleep(100);
+	safe_write(&mother->philo[0], DEAD);
+}
+
+int main(int ac, char **av)
+{
+	t_gen mother;
+	int i;
 
 	if (ac < 5 || ac > 6)
 		error(&mother, 1);
 	check_args(&mother, av, ac);
 	fill_mother(&mother, av, ac);
 	sit_down_philos(&mother);
-	i = 0;
-	while (i < mother.nb_philos)
+	if (mother.nb_philos == 1)
+		one_philo(&mother);
+	else
 	{
-		pthread_create(&mother.philo[i].thread,
-			NULL, &dinner_time, &mother.philo[i]);
-		if (mother.nb_philos % 2 == 1)
-			usleep(100);
-		i++;
-	}
-	check_if_dead_or_done_eating(&mother);
-	i = 0;
-	while (i < mother.nb_philos)
-	{
-		pthread_join(mother.philo[i].thread, NULL);
-		i++;
+		i = 0;
+		while (i < mother.nb_philos)
+		{
+			pthread_create(&mother.philo[i].thread,
+						   NULL, &dinner_time, &mother.philo[i]);
+			if (mother.nb_philos % 2 == 1)
+				usleep(100);
+			i++;
+		}
+		check_if_dead_or_done_eating(&mother);
+		i = 0;
+		while (i < mother.nb_philos)
+		{
+			pthread_join(mother.philo[i].thread, NULL);
+			i++;
+		}
 	}
 	return (0);
 }
